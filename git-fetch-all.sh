393d3be9dff3395626d08a3639e38a155b74c452
Skye Tsui <skyetsui@gmail.com>
2019-05-13 13:14:04 +0800

    Fetch if and only if valid git repo

--- git-fetch-all.sh
+++ git-fetch-all.sh
@@ -8,6 +8,8 @@ else
 fi
 for D in $(find $DIR -name .git); do
     echo $D
-    git --git-dir=$D fetch
+    if git --git-dir=$D rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
+        git --git-dir=$D fetch
+    fi
     sleep 1 # sleep to avoid server block
 done
