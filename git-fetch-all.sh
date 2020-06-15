c14c576620c88d2d7cad890fa9bb16aa10fd359d
skyetsui <skyetsui@gmail.com>
2020-06-15 14:25:11 +0800

    Add options all and prune to the fetch command

--- git-fetch-all.sh
+++ git-fetch-all.sh
@@ -9,7 +9,7 @@ fi
 for D in $(find $DIR -name .git); do
     if git --git-dir=$D rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
         echo $D
-        git --git-dir=$D fetch
+        git --git-dir=$D fetch --all --prune
     fi
     sleep 1 # sleep to avoid server block
 done
